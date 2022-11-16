8e7e06aedc6bf1ba25f4d69c49ed7f648b5f523b
Hendrik Belitz <hendrik@innovation-through-understanding.de>
2022-11-16 14:16:43 +0100

    Unit is now a nominal type instead of a simple replacement for true.

--- src/utilityTypes/Unit.ts
+++ src/utilityTypes/Unit.ts
@@ -1,8 +1,10 @@
+import type { Nominal } from "./Nominal";
+
 /** 
  * Unit type for functional programming. For simplicity and minimal space consumption, this maps to `true` 
  * @important Note that this is different to `void` which basically is undefined and therefor *falsy*
  */
-export type Unit = true;
+export type Unit = Nominal<true, "unit">;
 
 /** Create a value of type Unit */
-export const unit = (): Unit => true;
+export const unit = (): Unit => true as Unit;
